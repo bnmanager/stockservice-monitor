fda59e87c7068673a7dc542d8c7644f7aa6f0270
Bill Mwaniki <16713211+bnmanager@users.noreply.github.com>
2020-08-18 22:23:19 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stockservice_monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
